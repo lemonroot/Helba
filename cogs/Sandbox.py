e2f5047646caccaa1a6d0359a35251b40989b389
Lemon Root <thelemonroot@gmail.com>
2021-02-28 01:09:00 -0500

    Sandbox character creation test
    Created a test for writing/storing character info.

--- cogs/Sandbox.py
+++ cogs/Sandbox.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 import json
 
@@ -18,13 +19,39 @@ class Sandbox(commands.Cog):
         self._last_member = member
 
     @commands.command(name='DM')
-    async def on_message(self, ctx, *, member: discord.Member = None):
+    async def dm_user(self, ctx, *, member: discord.Member = None):
         member = member or ctx.author
         if member == self.bot.user:
             return
 
         # me = await ctx.fetch_user_profile(ctx.author.id)
         await member.send('Nice')
+
+    @commands.command(name='create')
+    async def create(self, ctx):
+        directory = ('players/' + str(ctx.author.id) + '/data.json')
+        if not os.path.exists('players/{}'.format(ctx.author.id)):
+            os.makedirs('players/{}'.format(ctx.author.id))
+            new_account = {
+                'owner': ctx.author.id,
+                'race': 'None',
+                'job': 'None',
+                'deity': 'None',
+                'skill': 'None',
+                'lvl': '1',
+                'exp': '0',
+                'weapon': 'None',
+                'armor': 'None',
+                'accessory': 'None'
+            }
+
+            with open(directory, 'w') as f:
+                json.dump(new_account, f)
+
+            print('created')
+        else:
+            print('exists')
+        # json.read(directory)
 
 
 def setup(bot):
